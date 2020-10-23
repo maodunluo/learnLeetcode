8e0c3e6d8fcb3f5391e3c427cac7bf7356952030
maodunluo <yyc05060708@gmail.com>
2020-10-23 23:05:22 +0800

    feature#626 增加union实现的逻辑

--- src/main/java/learnSql/626/ExchangeSeats.sql
+++ src/main/java/learnSql/626/ExchangeSeats.sql
@@ -14,3 +14,15 @@ FROM seat as i;
 --          WHEN id % 2 != 0 AND id < SELECT MAX(id) from seat THEN (SELECT student FROM seat WHERE id = (i.id + 1)) ELSE
 --              student END) AS student
 -- FROM seat as i;
+
+-- The MySQL UNION operator is used to combine the result sets of 2 or more SELECT statements.
+-- It removes duplicate rows between the various SELECT statements.
+-- Each SELECT statement within the UNION operator must have the same number of fields in the result sets with similar data types.
+
+-- 这个如果嵌套查student就会报错，应该怎么改？？
+SELECT id, student FROM seat WHERE id % 2 != 0 AND id = (SELECT MAX(id) FROM seat)
+UNION
+SELECT id - 1 AS id, student FROM seat WHERE id % 2 = 0
+UNION
+SELECT id + 1 AS id, student FROM seat WHERE id % 2 != 0 AND id != (SELECT MAX(id) FROM seat)
+ORDER BY id ASC
