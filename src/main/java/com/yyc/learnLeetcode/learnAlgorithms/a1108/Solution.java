e21dbbd972c331a23b51759bd52fd40e9f9f77db
maodunluo <yyc05060708@gmail.com>
2020-10-26 14:56:00 +0800

    feature#1108 test the using of String.replace

---
+++ src/main/java/com/yyc/learnLeetcode/learnAlgorithms/a1108/Solution.java
@@ -0,0 +1,14 @@
+package com.yyc.learnLeetcode.learnAlgorithms.a1108;
+
+/**
+ * Defanging an IP Address
+ * Given a valid (IPv4) IP address, return a defanged version of that IP address.
+ * A defanged IP address replaces every period "." with "[.]".
+ *
+ * @author yuechao
+ */
+public class Solution {
+    public String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
+    }
+}
