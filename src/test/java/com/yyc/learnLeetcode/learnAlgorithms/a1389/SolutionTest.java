925cfc964f9518c2d165e8bb9065531faa0bddad
maodunluo <yyc05060708@gmail.com>
2020-10-29 16:41:02 +0800

    feature#1389 Create Target Array in the Given Order

---
+++ src/test/java/com/yyc/learnLeetcode/learnAlgorithms/a1389/SolutionTest.java
@@ -0,0 +1,16 @@
+package com.yyc.learnLeetcode.learnAlgorithms.a1389;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void createTargetArray() {
+        Solution solution = new Solution();
+        int[] except = {0, 4, 1, 3, 2};
+        int[] actual = solution.createTargetArray(new int[]{0, 1, 2, 3, 4}, new int[]{0, 1, 2, 2, 1});
+        assertArrayEquals(except, actual);
+    }
+}
