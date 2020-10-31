b41b6e2a6aeb8586955cbc865b56d19c8e711186
maodunluo <yyc05060708@gmail.com>
2020-10-31 15:05:26 +0800

    feature#1290 Convert Binary Number in a Linked List to Integer

---
+++ src/test/java/com/yyc/learnLeetcode/learnAlgorithms/a1290/SolutionTest.java
@@ -0,0 +1,18 @@
+package com.yyc.learnLeetcode.learnAlgorithms.a1290;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void getDecimalValue() {
+        Solution solution = new Solution();
+        ListNode listNode = new ListNode();
+        listNode.val = 0;
+        listNode.next = new ListNode(1);
+        listNode.next.next = new ListNode(1);
+        assertEquals(3,solution.getDecimalValue(listNode));
+    }
+}
