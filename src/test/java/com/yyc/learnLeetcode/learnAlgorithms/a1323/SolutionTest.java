0e2db35665ed8f240fc1e88576339cd16d8a0307
maodunluo <yyc05060708@gmail.com>
2020-11-01 15:51:01 +0800

    feature#1323 Maximum 69 Number

---
+++ src/test/java/com/yyc/learnLeetcode/learnAlgorithms/a1323/SolutionTest.java
@@ -0,0 +1,14 @@
+package com.yyc.learnLeetcode.learnAlgorithms.a1323;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void maximum69Number() {
+        Solution solution = new Solution();
+        assertEquals(9999, solution.maximum69Number(9996));
+    }
+}
