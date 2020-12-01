25c6717f2ff78118d9f68d468c1b163d4f9bc73d
maodunluo <yyc05060708@gmail.com>
2020-12-02 02:31:45 +0800

    feature#339 Nested List Weight Sum（一定要注意java参数的值传递问题，用自增自减运算符在递归中会导致问题）

---
+++ src/test/java/com/yyc/learnLeetcode/learnAlgorithms/a339/SolutionTest.java
@@ -0,0 +1,23 @@
+package com.yyc.learnLeetcode.learnAlgorithms.a339;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void depthSumTest() {
+        //todo 如何快速的mock出来复杂对象
+        Solution solution = new Solution();
+
+    }
+
+    public int depthSum(List<NestedInteger> nestedList) {
+
+        return 0;
+    }
+}
