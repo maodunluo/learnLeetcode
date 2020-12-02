d5096839893baa9426cee0c10b0423ccb0bada11
maodunluo <yyc05060708@gmail.com>
2020-12-02 17:06:39 +0800

    feature#728 Self Dividing Numbers

---
+++ src/main/java/com/yyc/learnLeetcode/learnAlgorithms/a728/Solution.java
@@ -0,0 +1,35 @@
+package com.yyc.learnLeetcode.learnAlgorithms.a728;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author yuechao
+ */
+public class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> numberLists = new LinkedList<>();
+        for (int i = left; i <= right; i++) {
+            if (verifySelfDividingNumber(i)) {
+                numberLists.add(i);
+            }
+        }
+        return numberLists;
+    }
+
+    public boolean verifySelfDividingNumber(int number) {
+        //如何得到各个位数上的值。
+        if (number < 10) {
+            return true;
+        }
+        int flag = number;
+        do {
+            int remainder = flag % 10;
+            if (remainder == 0 || number % remainder != 0) {
+                return false;
+            }
+            flag = flag / 10;
+        } while (flag != 0);
+        return true;
+    }
+}
