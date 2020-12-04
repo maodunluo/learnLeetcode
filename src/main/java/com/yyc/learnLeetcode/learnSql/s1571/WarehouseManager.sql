3149077bb8c2246e70965d669c52f91965c95040
maodunluo <yyc05060708@gmail.com>
2020-12-04 15:24:30 +0800

    feature#sql1571 Warehouse Manager

---
+++ src/main/java/com/yyc/learnLeetcode/learnSql/s1571/WarehouseManager.sql
@@ -0,0 +1,3 @@
+SELECT Warehouse.name AS warehouse_name, SUM(units * Width * Length * Height) AS  volume
+FROM Warehouse LEFT JOIN Products ON Warehouse.product_id = Products.product_id
+GROUP BY warehouse_name
