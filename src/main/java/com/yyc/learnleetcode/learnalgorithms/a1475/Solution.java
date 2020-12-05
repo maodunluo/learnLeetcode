3f0f10f2dfb5f0b84b7bac1043a3e41bf19021f0
maodunluo <yyc05060708@gmail.com>
2020-12-05 19:07:10 +0800

    feature#1475 Final Prices With a Special Discount in a Shop

---
+++ src/main/java/com/yyc/learnleetcode/learnalgorithms/a1475/Solution.java
@@ -0,0 +1,21 @@
+package com.yyc.learnleetcode.learnalgorithms.a1475;
+
+/**
+ * @author yuechao
+ */
+public class Solution {
+    public int[] finalPrices(int[] prices) {
+        int[] result = new int[prices.length];
+        for (int i = 0; i < prices.length - 1; i++) {
+            result[i] = prices[i];
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[j] <= prices[i]) {
+                    result[i] = prices[i] - prices[j];
+                    break;
+                }
+            }
+        }
+        result[prices.length - 1] = prices[prices.length - 1];
+        return result;
+    }
+}
---
+++ src/test/java/com/yyc/learnleetcode/learnalgorithms/a1475/SolutionTest.java
@@ -0,0 +1,15 @@
+package com.yyc.learnleetcode.learnalgorithms.a1475;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    int[] input = new int[]{8,4,6,2,3};
+    int[] expected = new int[]{4,2,4,2,3};
+    @Test
+    void finalPrices() {
+        assertArrayEquals(expected, new Solution().finalPrices(input));
+    }
+}
