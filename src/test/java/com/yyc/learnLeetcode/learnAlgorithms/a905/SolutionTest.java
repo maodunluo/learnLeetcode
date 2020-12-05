af07de06cbf834d998b61562aa7deef0f4eb8611
maodunluo <yyc05060708@gmail.com>
2020-12-05 16:16:04 +0800

    feature#905 Sort Array By Parity

---
+++ src/test/java/com/yyc/learnLeetcode/learnAlgorithms/a905/SolutionTest.java
@@ -0,0 +1,21 @@
+package com.yyc.learnLeetcode.learnAlgorithms.a905;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    int[] input = new int[]{3, 1, 2, 4};
+    int[] expected = new int[]{2, 4, 3, 1};
+
+    @Test
+    void sortArrayByParity() {
+        assertArrayEquals(expected, new Solution().sortArrayByParity(input));
+    }
+
+    @Test
+    void simpleSortArrayByParity() {
+        assertArrayEquals(expected, new Solution().simpleSortArrayByParity(input));
+    }
+}
