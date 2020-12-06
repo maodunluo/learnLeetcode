8723e700b24da0ad7d4f938eb2a251e2fd2ad579
maodunluo <yyc05060708@gmail.com>
2020-12-06 15:24:08 +0800

    fix#905 修改方法的副作用

--- src/main/java/com/yyc/learnleetcode/learnalgorithms/a905/Solution.java
+++ src/main/java/com/yyc/learnleetcode/learnalgorithms/a905/Solution.java
@@ -1,5 +1,7 @@
 package com.yyc.learnleetcode.learnalgorithms.a905;
 
+import java.util.Arrays;
+
 /**
  * @author yuechao
  */
@@ -23,8 +25,16 @@ public class Solution {
         return result;
     }
 
+    /**
+     *
+     * 这个要保持函数没有副作用，就不能用result = arr，因为上述赋值就会将result指向arr的地址，是浅拷贝，
+     * 修改result的话，arr里面的数值也会变化。可以用以下语句进行深拷贝。要确保它们对数据结构的任何修改对于调用者都是不可见的。
+     *
+     * @param arr 传入的input数组
+     * @return 排序后的数组
+     */
     public int[] simpleSortArrayByParity(int[] arr) {
-        int[] result = arr;
+        int[] result = Arrays.copyOf(arr, arr.length);
         for (int i = 0, j = 0; j < result.length; j++) {
             if (result[j] % 2 == 0) {
                 int temp = result[i];
--- src/test/java/com/yyc/learnleetcode/learnalgorithms/a905/SolutionTest.java
+++ src/test/java/com/yyc/learnleetcode/learnalgorithms/a905/SolutionTest.java
@@ -10,6 +10,8 @@ class SolutionTest {
 
     int[] input = new int[]{3, 1, 2, 4};
     int[] expected = new int[]{2, 4, 3, 1};
+    //这个是指向数组的引用是final修饰的，这个引用指向了一个地址，这个地址里的内容是可以改变的。
+    final int[] finalInput = new int[]{3, 1, 2, 4};
 
     @Test
     void sortArrayByParity() {
@@ -19,5 +21,10 @@ class SolutionTest {
     @Test
     void simpleSortArrayByParity() {
         assertArrayEquals(expected, new Solution().simpleSortArrayByParity(input));
+    }
+
+    @Test
+    void finalInputTest() {
+        assertArrayEquals(expected, new Solution().simpleSortArrayByParity(finalInput));
     }
 }
