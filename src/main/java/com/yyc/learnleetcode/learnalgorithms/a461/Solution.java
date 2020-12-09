67852756daf843ce25d358e81a3ba523bb7766af
maodunluo <yyc05060708@gmail.com>
2020-12-09 18:56:36 +0800

    feature#462 Hamming Distance

---
+++ src/main/java/com/yyc/learnleetcode/learnalgorithms/a461/Solution.java
@@ -0,0 +1,18 @@
+package com.yyc.learnleetcode.learnalgorithms.a461;
+
+/**
+ * @author yuechao
+ */
+public class Solution {
+    public int hammingDistance(int x, int y) {
+        int z = x ^ y;
+        int result = 0;
+        while (z != 0) {
+            if (z % 2 != 0) {
+                result++;
+            }
+            z /= 2;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/yyc/learnleetcode/learnalgorithms/a461/SolutionTest.java
@@ -0,0 +1,13 @@
+package com.yyc.learnleetcode.learnalgorithms.a461;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void hammingDistance() {
+        assertEquals(2, new Solution().hammingDistance(1, 4));
+    }
+}
