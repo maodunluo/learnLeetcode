1b722958d473f63f21fa88247f2ec51dbdb5b499
maodunluo <yyc05060708@gmail.com>
2020-12-09 16:35:13 +0800

    feature#657 Robot Return to Origin

---
+++ src/test/java/com/yyc/learnleetcode/learnalgorithms/a657/SolutionTest.java
@@ -0,0 +1,13 @@
+package com.yyc.learnleetcode.learnalgorithms.a657;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void judgeCircle() {
+        assertTrue(new Solution().judgeCircle("UD"));
+    }
+}
