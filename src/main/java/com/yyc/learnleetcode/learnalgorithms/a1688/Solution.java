bcb2d7c1f6e2794cc973f81fc6993752275f75d4
maodunluo <yyc05060708@gmail.com>
2021-01-14 23:42:25 +0800

    feature#1688. Count of Matches in Tournament

---
+++ src/main/java/com/yyc/learnleetcode/learnalgorithms/a1688/Solution.java
@@ -0,0 +1,21 @@
+package com.yyc.learnleetcode.learnalgorithms.a1688;
+
+/**
+ * 1688. Count of Matches in Tournament
+ *
+ * @author yuechao
+ */
+public class Solution {
+    public int numberOfMatches(int n) {
+        int result = 0;
+        while(n > 1) {
+            result += n /2;
+            if(n % 2 == 1) {
+                n = n / 2 + 1;
+            } else {
+                n /= 2;
+            }
+        }
+        return result;
+    }
+}
