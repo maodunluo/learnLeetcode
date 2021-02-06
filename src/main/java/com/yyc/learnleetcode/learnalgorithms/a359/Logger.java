9247fa7e7b0ad10a74c8d03a7441570dd32d753e
maodunluo <yyc05060708@gmail.com>
2021-02-06 12:29:36 +0800

    feature#359. Logger Rate Limiter

---
+++ src/main/java/com/yyc/learnleetcode/learnalgorithms/a359/Logger.java
@@ -0,0 +1,36 @@
+package com.yyc.learnleetcode.learnalgorithms.a359;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * 359. Logger Rate Limiter
+ *
+ * @author yuechao
+ */
+public class Logger {
+    Map<String, Integer> map;
+    private static final Integer INTERVAL = 10;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public Logger() {
+        map = new HashMap<>(32);
+    }
+
+    /**
+     * Returns true if the message should be printed in the given timestamp, otherwise returns false.
+     * If this method returns false, the message will not be printed.
+     * The timestamp is in seconds granularity.
+     */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        Integer integer = map.get(message);
+        if (Objects.isNull(integer) || timestamp - integer >= INTERVAL) {
+            map.put(message, timestamp);
+            return true;
+        }
+        return false;
+    }
+}
