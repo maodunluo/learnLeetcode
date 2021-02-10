689a45115a98ef6937c156302b1e173ccd6d63ed
maodunluo <yyc05060708@gmail.com>
2021-02-10 15:59:46 +0800

    feature#1217. Minimum Cost to Move Chips to The Same Position

---
+++ src/main/java/com/yyc/learnleetcode/learnalgorithms/a1217/Solution.java
@@ -0,0 +1,20 @@
+package com.yyc.learnleetcode.learnalgorithms.a1217;
+
+/**
+ * 1217. Minimum Cost to Move Chips to The Same Position
+ *
+ * @author yuechao
+ */
+public class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int[] positionArray = new int[2];
+        for (int number : position) {
+            if (number % 2 == 1) {
+                positionArray[1] += 1;
+            } else {
+                positionArray[0] += 1;
+            }
+        }
+        return Math.min(positionArray[0], positionArray[1]);
+    }
+}
