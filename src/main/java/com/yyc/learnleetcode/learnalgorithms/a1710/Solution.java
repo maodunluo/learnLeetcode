326acdbf2a6fa90a309941744b8c810dde6bab88
maodunluo <yyc05060708@gmail.com>
2021-02-27 11:11:23 +0800

    feature#1710. Maximum Units on a Truck

---
+++ src/main/java/com/yyc/learnleetcode/learnalgorithms/a1710/Solution.java
@@ -0,0 +1,26 @@
+package com.yyc.learnleetcode.learnalgorithms.a1710;
+
+import java.util.Arrays;
+
+/**
+ * 1710. Maximum Units on a Truck
+ *
+ * @author yuechao
+ */
+public class Solution {
+
+  public int maximumUnits(int[][] boxTypes, int truckSize) {
+    Arrays.sort(boxTypes, (boxType1, boxType2) -> boxType2[1] - boxType1[1]);
+    int result = 0;
+    for (int[] boxType : boxTypes) {
+      if (boxType[0] < truckSize) {
+        result += boxType[0] * boxType[1];
+        truckSize -= boxType[0];
+      } else {
+        result += truckSize * boxType[1];
+        break;
+      }
+    }
+    return result;
+  }
+}
