2c60b539b028852f21d860ca73f419f5a5d37ca5
maodunluo <yyc05060708@gmail.com>
2021-03-05 01:23:35 +0800

    feature#1777. Product's Price for Each Store

--- src/main/java/com/yyc/learnleetcode/learnSql/s1777/Product'sPriceForEachStore.sql
+++ src/main/java/com/yyc/learnleetcode/learnSql/s1777/Product'sPriceForEachStore.sql
@@ -20,6 +20,9 @@ VALUES ('1', 'store1', '70');
 INSERT INTO Products (product_id, store, price)
 VALUES ('1', 'store3', '80');
 
-SELECT product_id, SUM(IF(store = 'store1', price, null) )AS 'store1', SUM(IF(store = 'store2', price, null)) AS 'store2', SUM(IF(store = 'store3', price, null)) AS 'store3'
+SELECT product_id,
+       SUM(IF(store = 'store1', price, null)) AS 'store1',
+       SUM(IF(store = 'store2', price, null)) AS 'store2',
+       SUM(IF(store = 'store3', price, null)) AS 'store3'
 FROM Products
 GROUP BY product_id
