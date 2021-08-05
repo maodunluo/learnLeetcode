45e08e09e6d37b0742c3d39337e056c4c8958310
yangyc <yangyc@133.cn>
2021-08-05 18:48:03 +0800

    feature#1265. Print Immutable Linked List in Reverse

---
+++ src/main/java/com/yyc/learnleetcode/learnalgorithms/a1920/Solution.java
@@ -0,0 +1,20 @@
+package com.yyc.learnleetcode.learnalgorithms.a1920;
+
+/**
+ * 1920. Build Array from Permutation
+ *
+ * Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+ *
+ * A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+ *
+ * @author admin
+ */
+public class Solution {
+  public int[] buildArray(int[] nums) {
+    var resultArray = new int[nums.length];
+    for (var i = 0; i < nums.length; i++) {
+      resultArray[i] = nums[nums[i]];
+    }
+    return resultArray;
+  }
+}
