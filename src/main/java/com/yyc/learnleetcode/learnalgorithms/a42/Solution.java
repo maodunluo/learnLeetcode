ae4769aa61e0287ba0c6909d0236ea4fcd22287b
maodunluo <yyc05060708@gmail.com>
2022-01-18 19:51:17 +0800

    feature#42. Trapping Rain Water(how to improve this solution?)

---
+++ src/main/java/com/yyc/learnleetcode/learnalgorithms/a42/Solution.java
@@ -0,0 +1,42 @@
+package com.yyc.learnleetcode.learnalgorithms.a42;
+
+/**
+ * 42. Trapping Rain Water
+ *
+ * @author yuechao
+ */
+public class Solution {
+
+  public int trap(int[] height) {
+    int result = 0;
+    for (int i = 0; i < height.length; i++) {
+      final int min = Math.min(getLeftMax(i, height),
+          getRightMax(i, height));
+      if (min != 0) {
+        result += (min - height[i]);
+      }
+    }
+    return result;
+  }
+
+  private int getLeftMax(int index, int[] height) {
+    int result = 0;
+    for (int i = index; i >= 0; i--) {
+      if (height[i] > result) {
+        result = height[i];
+      }
+    }
+    return result;
+  }
+
+  private int getRightMax(int index, int[] height) {
+    int result = 0;
+    for (int i = index; i < height.length; i++) {
+      if (height[i] > result) {
+        result = height[i];
+      }
+    }
+    return result;
+  }
+
+}
