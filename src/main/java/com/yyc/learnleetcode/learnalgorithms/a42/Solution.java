f486f6c15e0e17f71ff5d3aa79ad6524795315a0
maodunluo <yyc05060708@gmail.com>
2022-01-19 23:38:36 +0800

    feature#42. Trapping Rain Water

--- src/main/java/com/yyc/learnleetcode/learnalgorithms/a42/Solution.java
+++ src/main/java/com/yyc/learnleetcode/learnalgorithms/a42/Solution.java
@@ -38,4 +38,42 @@ public class Solution {
     return result;
   }
 
+  public int trapV2(int[] height) {
+    // 找出该节点左边最大的数组
+    int[] leftMaxArray = new int[height.length];
+    // 该节点右边最大的元素组成的数组。
+    int[] rightMaxArray = new int[height.length];
+    leftMaxArray[0] = height[0];
+    for (int i = 1; i < height.length; i++) {
+      leftMaxArray[i] = Math.max(height[i], leftMaxArray[i - 1]);
+    }
+    rightMaxArray[height.length - 1] = height[height.length - 1];
+    for (int i = height.length - 2; i >= 0; i--) {
+      rightMaxArray[i] = Math.max(height[i], rightMaxArray[i + 1]);
+    }
+    int result = 0;
+    for (int i = 0; i < height.length; i++) {
+      result += (Math.min(leftMaxArray[i], rightMaxArray[i]) - height[i]);
+    }
+    return result;
+  }
+
+  public int trapV3(int[] height) {
+    int result = 0;
+    int leftIndex = 0;
+    int rightIndex = height.length - 1;
+    int maxLeft = height[0];
+    int maxRight = height[height.length - 1];
+    while (leftIndex < rightIndex) {
+      if (maxLeft < maxRight) {
+        result += (maxLeft - height[leftIndex]);
+        maxLeft = Math.max(maxLeft, height[++leftIndex]);
+      } else {
+        result += (maxRight - height[rightIndex]);
+        maxRight = Math.max(maxRight, height[--rightIndex]);
+      }
+    }
+    return result;
+  }
+
 }
