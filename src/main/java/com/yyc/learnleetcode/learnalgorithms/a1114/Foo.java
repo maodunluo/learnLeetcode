893fe19d786348311ac96c1ec33ba3173b705b26
maodunluo <yyc05060708@gmail.com>
2022-10-25 21:19:13 +0800

    feature#1114. Print in Order

---
+++ src/main/java/com/yyc/learnleetcode/learnalgorithms/a1114/Foo.java
@@ -0,0 +1,30 @@
+package com.yyc.learnleetcode.learnalgorithms.a1114;
+
+/**
+ * 1114. Print in Order
+ */
+public class Foo {
+
+  public Foo() {
+
+  }
+
+  public void first(Runnable printFirst) throws InterruptedException {
+
+    // printFirst.run() outputs "first". Do not change or remove this line.
+    printFirst.run();
+  }
+
+  public void second(Runnable printSecond) throws InterruptedException {
+
+    // printSecond.run() outputs "second". Do not change or remove this line.
+    printSecond.run();
+  }
+
+  public void third(Runnable printThird) throws InterruptedException {
+
+    // printThird.run() outputs "third". Do not change or remove this line.
+    printThird.run();
+  }
+
+}
